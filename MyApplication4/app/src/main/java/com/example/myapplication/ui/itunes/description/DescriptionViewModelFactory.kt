805aae85f685bc50659c72f1ea7670e608c1d329
fyrost <fmga.webbie@gmail.com>
2019-08-07 20:55:28 +0800

    [:wrench:] additional injection config

--- MyApplication4/app/src/main/java/com/example/myapplication/ui/itunes/description/DescriptionViewModelFactory.kt
+++ MyApplication4/app/src/main/java/com/example/myapplication/ui/itunes/description/DescriptionViewModelFactory.kt
@@ -1,15 +1,17 @@
 package com.example.myapplication.ui.itunes.description
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.myapplication.data.repository.DatabaseRepository
 
 @Suppress("UNCHECKED_CAST")
 class DescriptionViewModelFactory(
-    private val databaseRepository: DatabaseRepository
+    private val databaseRepository: DatabaseRepository,
+    private val appContext: Context
 ): ViewModelProvider.NewInstanceFactory() {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        return DescriptionViewModel(databaseRepository) as T
+        return DescriptionViewModel(databaseRepository,appContext) as T
     }
 }
