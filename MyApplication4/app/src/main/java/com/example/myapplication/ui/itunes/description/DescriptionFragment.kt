3b8fb92180a4c1346b748a2bc8ea3e19b50d18d8
fyrost <fmga.webbie@gmail.com>
2019-08-07 20:56:48 +0800

    [:wrench:] databinding description page

--- MyApplication4/app/src/main/java/com/example/myapplication/ui/itunes/description/DescriptionFragment.kt
+++ MyApplication4/app/src/main/java/com/example/myapplication/ui/itunes/description/DescriptionFragment.kt
@@ -6,10 +6,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 
 import com.example.myapplication.R
 import com.example.myapplication.data.db.entity.ITunesResult
+import com.example.myapplication.databinding.DescriptionFragmentBinding
 import kotlinx.android.synthetic.main.description_fragment.*
 import org.kodein.di.KodeinAware
 import org.kodein.di.android.x.closestKodein
@@ -25,25 +27,29 @@ class DescriptionFragment : Fragment(), KodeinAware {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.description_fragment, container, false)
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+            .get(DescriptionViewModel::class.java)
+        val binding  = DataBindingUtil.inflate<DescriptionFragmentBinding>(inflater, R.layout.description_fragment,container,false)
+            .apply {
+                this.lifecycleOwner = this@DescriptionFragment
+                this.viewmodel = viewModel
+            }
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-
-        viewModel = ViewModelProviders.of(this, viewModelFactory)
-            .get(DescriptionViewModel::class.java)
-
         val safeArgs = arguments?.let { DescriptionFragmentArgs.fromBundle(it) }
         iTunesResult = safeArgs?.iTunesResult!!
-        bindUI()
+        viewModel.detailDescription(iTunesResult)
+//        bindUI()
     }
 
     private fun bindUI() {
         textView_description_title.text = iTunesResult.trackName
 
         fab.setOnClickListener {
-            viewModel.insertResult(iTunesResult)
+            viewModel.insertResult()
         }
     }
 
--- MyApplication4/app/src/main/java/com/example/myapplication/ui/itunes/description/DescriptionViewModel.kt
+++ MyApplication4/app/src/main/java/com/example/myapplication/ui/itunes/description/DescriptionViewModel.kt
@@ -1,16 +1,90 @@
 package com.example.myapplication.ui.itunes.description
 
+import android.content.Context
+import android.widget.ImageView
+import androidx.databinding.Bindable
+import androidx.databinding.BindingAdapter
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.bumptech.glide.Glide
 import com.example.myapplication.data.db.entity.ITunesResult
 import com.example.myapplication.data.repository.DatabaseRepository
+import org.threeten.bp.OffsetDateTime
+import org.threeten.bp.Year
 
 class DescriptionViewModel(
-    private val databaseRepository: DatabaseRepository
+    private val databaseRepository: DatabaseRepository,
+    private val appContext: Context
 ) : ViewModel() {
 
+    private lateinit var result: ITunesResult
+    
+    private val _kind = MutableLiveData<String>()
+    val kind: LiveData<String>
+        get() = _kind
+    private val _trackName = MutableLiveData<String>()
+    val trackName: LiveData<String>
+        get() = _trackName
+    private val _artworkUrl100 = MutableLiveData<String>()
+    val artworkUrl100: LiveData<String>
+        get() = _artworkUrl100
+    private val _primaryGenreName = MutableLiveData<String>()
+    val primaryGenreName: LiveData<String>
+        get() = _primaryGenreName
+    private val _releaseDate = MutableLiveData<String>()
+    val releaseDate: LiveData<String>
+        get() = _releaseDate
+    private val _trackPrice = MutableLiveData<String>()
+    val trackPrice: LiveData<String>
+        get() = _trackPrice
+    private val _artistName = MutableLiveData<String>()
+    val artistName: LiveData<String>
+        get() = _artistName
+    private val _longDescription = MutableLiveData<String>()
+    val longDescription: LiveData<String>
+        get() = _longDescription
+    private val _collectionName = MutableLiveData<String>()
+    val collectionName: LiveData<String>
+        get() = _collectionName
+    private val _collectionPrice = MutableLiveData<String>()
+    val collectionPrice: LiveData<String>
+        get() = _collectionPrice
 
-    fun insertResult(iTunesResult: ITunesResult) {
-        databaseRepository.insertResult(iTunesResult)
+    fun insertResult() {
+        databaseRepository.insertResult(result)
+    }
+
+    fun detailDescription(iTunesResult: ITunesResult){
+        result = iTunesResult
+        _kind.value = iTunesResult.kind?.capitalize()
+        _trackName.value = iTunesResult.trackName
+        _artworkUrl100.value = iTunesResult.artworkUrl100
+        _primaryGenreName.value = iTunesResult.primaryGenreName
+        _releaseDate.value = getYear(iTunesResult.releaseDate)
+        _trackPrice.value = priceFormat(iTunesResult.trackPrice)
+        _artistName.value = iTunesResult.artistName
+        _longDescription.value = iTunesResult.longDescription
+        _collectionName.value = iTunesResult.collectionName
+        _collectionPrice.value = priceFormat(iTunesResult.collectionPrice)
+
+        if(iTunesResult.kind=="song" || iTunesResult.kind=="music-video"){
+
+        }
+    }
+    @BindingAdapter("app:imageUrl")
+    fun loadImage(view: ImageView, url: String) {
+        Glide.with(view.context).load(url).into(view)
+    }
+
+    private fun getYear(string : String?) : String {
+        return OffsetDateTime.parse(string).year.toString()
+    }
+
+    private fun priceFormat(double : Double?) : String {
+        if (double != null) {
+            if(double > 0) return "$ $double"
+        }
+        return "FREE"
     }
 }
