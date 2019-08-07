ba2538c34ad9455092bc62e3db8c99166023d0f2
ryantesoro <ryantesoro@yahoo.com>
2019-08-07 12:08:38 +0800

    [:lipstick:] Added artwork to recyclerView item

--- MyApplication4/app/src/main/java/com/example/myapplication/ui/itunes/favorite/FavoriteItem.kt
+++ MyApplication4/app/src/main/java/com/example/myapplication/ui/itunes/favorite/FavoriteItem.kt
@@ -1,19 +1,24 @@
 package com.example.myapplication.ui.itunes.favorite
 
+import com.bumptech.glide.Glide
 import com.example.myapplication.R
 import com.example.myapplication.data.db.entity.ITunesResult
 import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
 import kotlinx.android.synthetic.main.recycler_view_item.*
+import kotlinx.android.synthetic.main.recycler_view_item.view.*
 
 class FavoriteItem(
     val iTunesResult: ITunesResult
 ): Item() {
     override fun bind(viewHolder: ViewHolder, position: Int) {
-        if (!iTunesResult.trackName.isNullOrBlank()) {
-            viewHolder.apply {
-                textView_title.text = iTunesResult.trackName
-            }
+        viewHolder.apply {
+            textView_title.text = iTunesResult.trackName
+            textView_genre.text = iTunesResult.primaryGenreName
+            textView_price.text = iTunesResult.trackPrice.toString()
+            Glide.with(itemView.context)
+                .load(iTunesResult.artworkUrl100)
+                .into(itemView.imageView_Art)
         }
     }
 
