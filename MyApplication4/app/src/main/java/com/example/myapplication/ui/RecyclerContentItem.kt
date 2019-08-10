1e14add54374763610546a5117c277e0de016309
fyrost <fmga.webbie@gmail.com>
2019-08-10 19:26:19 +0800

    [:fire:] removed BrowseItem and FavoriteItem class and created a single class RecyclerContentItem

--- MyApplication4/app/src/main/java/com/example/myapplication/ui/RecyclerContentItem.kt
+++ MyApplication4/app/src/main/java/com/example/myapplication/ui/RecyclerContentItem.kt
@@ -1,20 +1,19 @@
-package com.example.myapplication.ui.itunes.favorite
-
+package com.example.myapplication.ui
 
 import com.bumptech.glide.Glide
 
 import com.example.myapplication.R
 import com.example.myapplication.data.db.entity.ITunesResult
+import com.example.myapplication.ui.utils.largerImage
 import com.example.myapplication.ui.utils.priceFormat
 
-import com.xwray.groupie.kotlinandroidextensions.Item
 import com.xwray.groupie.kotlinandroidextensions.ViewHolder
+import com.xwray.groupie.kotlinandroidextensions.Item
 
 import kotlinx.android.synthetic.main.recycler_view_item.*
 import kotlinx.android.synthetic.main.recycler_view_item.view.*
 
-
-class FavoriteItem(
+class RecyclerContentItem(
     val iTunesResult: ITunesResult
 ): Item() {
     override fun bind(viewHolder: ViewHolder, position: Int) {
@@ -23,10 +22,12 @@ class FavoriteItem(
             textView_genre.text = iTunesResult.primaryGenreName
             textView_price.text = iTunesResult.trackPrice.priceFormat()
             Glide.with(itemView.context)
-                .load(iTunesResult.artworkUrl100)
+                .load(iTunesResult.artworkUrl100.largerImage(200))
                 .into(itemView.imageView_Art)
         }
     }
 
     override fun getLayout() = R.layout.recycler_view_item
+
+    override fun getSpanSize(spanCount: Int, position: Int) = spanCount/3
 }
