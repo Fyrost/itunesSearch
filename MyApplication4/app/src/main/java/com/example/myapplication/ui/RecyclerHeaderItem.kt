74ae1501f71e8b50236ac8f7955a378d7efb7991
fyrost <fmga.webbie@gmail.com>
2019-08-10 18:21:18 +0800

    [:sparkles:] adding header to recycler view

---
+++ MyApplication4/app/src/main/java/com/example/myapplication/ui/RecyclerHeaderItem.kt
@@ -0,0 +1,38 @@
+package com.example.myapplication.ui
+
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import com.example.myapplication.R
+import com.xwray.groupie.kotlinandroidextensions.Item
+import com.xwray.groupie.kotlinandroidextensions.ViewHolder
+import kotlinx.android.synthetic.main.recycler_view_header.*
+
+
+class RecyclerHeaderItem(private val message: String,private val isRecyclerEmpty: Boolean): Item() {
+
+    override fun bind(viewHolder: ViewHolder, position: Int) {
+        viewHolder.apply {
+            if(isRecyclerEmpty) {
+                header_container.apply {
+                    layoutParams.height = ViewGroup.LayoutParams.MATCH_PARENT
+                    gravity = Gravity.CENTER
+                }
+                recycler_header.apply {
+                    gravity = Gravity.CENTER
+                    textSize = 22F
+                }
+            } else {
+                header_container.layoutParams.height = ViewGroup.LayoutParams.WRAP_CONTENT
+                recycler_header.apply {
+                    gravity = Gravity.START
+                    textSize = 16F
+                }
+            }
+            recycler_header.text = message
+        }
+    }
+
+    override fun getLayout() = R.layout.recycler_view_header
+
+}
